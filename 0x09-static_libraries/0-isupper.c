9e75f60a690e809cdf801f5462b8d0ddd7d0397b
dvmtuppence <36967068+dvmtuppence@users.noreply.github.com>
2022-04-01 18:16:04 -0700

    static_libraries

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,16 +1,11 @@
 #include "main.h"
-/**
- * _memset - fills memory with a constant byte,
- * @s: memory area.
- * @b: constant byte.
- * @n: bytes filled.
- * Return: the pointer to dest.
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-unsigned int i;
 
-	for (i = 0; i < n; i++)
-		*(s + i) =  b;
-	return (s);
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
 }
